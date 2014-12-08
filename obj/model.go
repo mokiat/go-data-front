275c552a291ad8444b4003e7357494fe297154af
Momchil Atanasov <momchil.dev@gmail.com>
2014-12-08 22:16:01 +0200

    Add initial OBJ model

---
+++ obj/model.go
@@ -0,0 +1,50 @@
+package model
+
+type Normal struct {
+	X float32
+	Y float32
+	Z float32
+}
+
+type Vertex struct {
+	X float32
+	Y float32
+	Z float32
+}
+
+type TexCoord struct {
+	U float32
+	V float32
+	W float32
+}
+
+const UndefinedIndex int = -1
+
+type Reference struct {
+	VertexIndex   int
+	NormalIndex   int
+	TexCoordIndex int
+	TexCoordSize  int
+}
+
+type Face struct {
+	References []Reference
+}
+
+type Mesh struct {
+	Faces        []Face
+	MaterialName string
+}
+
+type Object struct {
+	Meshes []Mesh
+	Name   string
+}
+
+type Model struct {
+	Vertices          []Vertex
+	Normals           []Normal
+	TexCoords         []TexCoord
+	Objects           []Object
+	MaterialLibraries []string
+}
