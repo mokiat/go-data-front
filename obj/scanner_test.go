8fedfec72636708b68b44490cac9946268b2a54e
Momchil Atanasov <momchil.dev@gmail.com>
2014-12-20 16:55:37 +0200

    Refactor tests

--- obj/scanner_test.go
+++ obj/scanner_test.go
@@ -4,68 +4,22 @@ import (
 	"os"
 
 	. "github.com/momchil-atanasov/go-data-front/obj"
-	"github.com/momchil-atanasov/go-data-front/obj/fakes"
+	"github.com/momchil-atanasov/go-data-front/obj/test_helpers"
 
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Scanner", func() {
 
-	var handler *fakes.FakeScannerHandler
+	var handlerFixture *test_helpers.HandlerFixture
 	var scanner Scanner
 
 	var scanError error
-	var commentCounter int
-	var vertexStartCounter int
-	var vertexXCounter int
-	var vertexYCounter int
-	var vertexZCounter int
-	var vertexEndCounter int
-	var texCoordStartCounter int
-	var texCoordEndCounter int
-	var texCoordUCounter int
-	var texCoordVCounter int
-	var texCoordWCounter int
-	var normalCounter int
-	var objectCounter int
-	var faceStartCounter int
-	var faceEndCounter int
-	var materialLibraryCounter int
-	var materialReferenceCounter int
-	var coordReferenceStartCounter int
-	var coordReferenceEndCounter int
-	var vertexIndexCounter int
-	var texCoordIndexCounter int
-	var normalIndexCounter int
 
 	BeforeEach(func() {
-		handler = new(fakes.FakeScannerHandler)
-		scanner = NewScanner(handler)
+		handlerFixture = test_helpers.NewHandlerFixture()
+		scanner = NewScanner(handlerFixture.Handler())
 		scanError = nil
-
-		commentCounter = 0
-		vertexStartCounter = 0
-		vertexEndCounter = 0
-		vertexXCounter = 0
-		vertexYCounter = 0
-		vertexZCounter = 0
-		texCoordStartCounter = 0
-		texCoordEndCounter = 0
-		texCoordUCounter = 0
-		texCoordVCounter = 0
-		texCoordWCounter = 0
-		normalCounter = 0
-		objectCounter = 0
-		faceStartCounter = 0
-		faceEndCounter = 0
-		materialLibraryCounter = 0
-		materialReferenceCounter = 0
-		coordReferenceStartCounter = 0
-		coordReferenceEndCounter = 0
-		vertexIndexCounter = 0
-		texCoordIndexCounter = 0
-		normalIndexCounter = 0
 	})
 
 	scanFile := func(filename string) {
@@ -77,212 +31,71 @@ var _ = Describe("Scanner", func() {
 		scanError = scanner.Scan(file)
 	}
 
-	assertComment := func(expectedComment string) {
-		Ω(handler.OnCommentCallCount()).Should(BeNumerically(">", commentCounter))
-		Ω(handler.OnCommentArgsForCall(commentCounter)).Should(Equal(expectedComment))
-		commentCounter++
-	}
-
-	assertVertexStart := func() {
-		Ω(handler.OnVertexStartCallCount()).Should(BeNumerically(">", vertexStartCounter))
-		vertexStartCounter++
-	}
-
-	assertVertexEnd := func() {
-		Ω(handler.OnVertexEndCallCount()).Should(BeNumerically(">", vertexEndCounter))
-		vertexEndCounter++
-	}
-
-	assertVertexX := func(expectedX float32) {
-		Ω(handler.OnVertexXCallCount()).Should(BeNumerically(">", vertexXCounter))
-		Ω(handler.OnVertexXArgsForCall(vertexXCounter)).Should(BeNumerically("~", expectedX, 0.0001))
-		vertexXCounter++
-	}
-
-	assertVertexY := func(expectedY float32) {
-		Ω(handler.OnVertexYCallCount()).Should(BeNumerically(">", vertexYCounter))
-		Ω(handler.OnVertexYArgsForCall(vertexYCounter)).Should(BeNumerically("~", expectedY, 0.0001))
-		vertexYCounter++
-	}
-
-	assertVertexZ := func(expectedZ float32) {
-		Ω(handler.OnVertexZCallCount()).Should(BeNumerically(">", vertexZCounter))
-		Ω(handler.OnVertexZArgsForCall(vertexZCounter)).Should(BeNumerically("~", expectedZ, 0.0001))
-		vertexZCounter++
-	}
-
-	assertVertexXYZ := func(expectedX, expectedY, expectedZ float32) {
-		assertVertexStart()
-		assertVertexX(expectedX)
-		assertVertexY(expectedY)
-		assertVertexZ(expectedZ)
-		assertVertexEnd()
-	}
-
-	assertTexCoordStart := func() {
-		Ω(handler.OnTexCoordStartCallCount()).Should(BeNumerically(">", texCoordStartCounter))
-		texCoordStartCounter++
-	}
-
-	assertTexCoordU := func(expectedU float32) {
-		Ω(handler.OnTexCoordUCallCount()).Should(BeNumerically(">", texCoordUCounter))
-		Ω(handler.OnTexCoordUArgsForCall(texCoordUCounter)).Should(BeNumerically("~", expectedU, 0.0001))
-		texCoordUCounter++
-	}
-
-	assertTexCoordV := func(expectedV float32) {
-		Ω(handler.OnTexCoordVCallCount()).Should(BeNumerically(">", texCoordVCounter))
-		Ω(handler.OnTexCoordVArgsForCall(texCoordVCounter)).Should(BeNumerically("~", expectedV, 0.0001))
-		texCoordVCounter++
-	}
-
-	assertTexCoordEnd := func() {
-		Ω(handler.OnTexCoordEndCallCount()).Should(BeNumerically(">", texCoordEndCounter))
-		texCoordEndCounter++
-	}
-
-	assertTexCoordUV := func(expectedU, expectedV float32) {
-		assertTexCoordStart()
-		assertTexCoordU(expectedU)
-		assertTexCoordV(expectedV)
-		assertTexCoordEnd()
-	}
-
-	assertNormal := func(expectedX, expectedY, expectedZ float32) {
-		Ω(handler.OnNormalCallCount()).Should(BeNumerically(">", normalCounter))
-		argX, argY, argZ := handler.OnNormalArgsForCall(normalCounter)
-		Ω(argX).Should(BeNumerically("~", expectedX, 0.0001))
-		Ω(argY).Should(BeNumerically("~", expectedY, 0.0001))
-		Ω(argZ).Should(BeNumerically("~", expectedZ, 0.0001))
-		normalCounter++
-	}
-
-	assertObject := func(expectedName string) {
-		Ω(handler.OnObjectCallCount()).Should(BeNumerically(">", objectCounter))
-		argName := handler.OnObjectArgsForCall(objectCounter)
-		Ω(argName).Should(Equal(expectedName))
-		objectCounter++
-	}
-
-	assertFaceStart := func() {
-		Ω(handler.OnFaceStartCallCount()).Should(BeNumerically(">", faceStartCounter))
-		faceStartCounter++
-	}
-
-	assertFaceEnd := func() {
-		Ω(handler.OnFaceEndCallCount()).Should(BeNumerically(">", faceEndCounter))
-		faceEndCounter++
-	}
-
-	assertMaterialLibrary := func(expectedPath string) {
-		Ω(handler.OnMaterialLibraryCallCount()).Should(BeNumerically(">", materialLibraryCounter))
-		pathArg := handler.OnMaterialLibraryArgsForCall(materialLibraryCounter)
-		Ω(pathArg).Should(Equal(expectedPath))
-		materialLibraryCounter++
-	}
-
-	assertMaterialReference := func(expectedName string) {
-		Ω(handler.OnMaterialReferenceCallCount()).Should(BeNumerically(">", materialReferenceCounter))
-		nameArg := handler.OnMaterialReferenceArgsForCall(materialReferenceCounter)
-		Ω(nameArg).Should(Equal(expectedName))
-		materialReferenceCounter++
-	}
-
-	assertCoordReferenceStart := func() {
-		Ω(handler.OnCoordReferenceStartCallCount()).Should(BeNumerically(">", coordReferenceStartCounter))
-		coordReferenceStartCounter++
-	}
-
-	assertCoordReferenceEnd := func() {
-		Ω(handler.OnCoordReferenceEndCallCount()).Should(BeNumerically(">", coordReferenceEndCounter))
-		coordReferenceEndCounter++
-	}
-
-	assertVertexIndex := func(expectedIndex int) {
-		Ω(handler.OnVertexIndexCallCount()).Should(BeNumerically(">", vertexIndexCounter))
-		argIndex := handler.OnVertexIndexArgsForCall(vertexIndexCounter)
-		Ω(argIndex).Should(Equal(expectedIndex))
-		vertexIndexCounter++
-	}
-
-	assertTexCoordIndex := func(expectedIndex int) {
-		Ω(handler.OnTexCoordIndexCallCount()).Should(BeNumerically(">", texCoordIndexCounter))
-		argIndex := handler.OnTexCoordIndexArgsForCall(texCoordIndexCounter)
-		Ω(argIndex).Should(Equal(expectedIndex))
-		texCoordIndexCounter++
-	}
-
-	assertNormalIndex := func(expectedIndex int) {
-		Ω(handler.OnNormalIndexCallCount()).Should(BeNumerically(">", normalIndexCounter))
-		argIndex := handler.OnNormalIndexArgsForCall(normalIndexCounter)
-		Ω(argIndex).Should(Equal(expectedIndex))
-		normalIndexCounter++
-	}
-
 	Context("when a basic OBJ file is scanned", func() {
 		BeforeEach(func() {
 			scanFile("testres/valid_basic.obj")
 		})
 
 		It("should have scanned the comments", func() {
-			assertComment("This is the beginning of this OBJ file.")
+			handlerFixture.AssertCommentCall("This is the beginning of this OBJ file.")
+			handlerFixture.AssertNoMoreCommentCalls()
 		})
 
 		It("should have scanned the vertices", func() {
-			assertVertexXYZ(-1.0, 1.0, -1.0)
-			assertVertexXYZ(-1.0, -1.0, 1.0)
-			assertVertexXYZ(1.0, -1.0, -1.0)
-			assertVertexXYZ(1.0, 1.0, 1.0)
+			handlerFixture.AssertVertexXYZ(-1.0, 1.0, -1.0)
+			handlerFixture.AssertVertexXYZ(-1.0, -1.0, 1.0)
+			handlerFixture.AssertVertexXYZ(1.0, -1.0, -1.0)
+			handlerFixture.AssertVertexXYZ(1.0, 1.0, 1.0)
 		})
 
 		It("should have scanned the texture coordinates", func() {
-			assertTexCoordUV(0.0, 0.0)
-			assertTexCoordUV(1.0, 1.0)
-			assertTexCoordUV(1.0, 0.0)
-			assertTexCoordUV(0.0, 1.0)
+			handlerFixture.AssertTexCoordUV(0.0, 0.0)
+			handlerFixture.AssertTexCoordUV(1.0, 1.0)
+			handlerFixture.AssertTexCoordUV(1.0, 0.0)
+			handlerFixture.AssertTexCoordUV(0.0, 1.0)
 		})
 
 		It("should have scanned the normals", func() {
-			assertNormal(0.0, 1.0, 0.0)
-			assertNormal(1.0, 0.0, 0.0)
-			assertNormal(0.0, 0.0, 1.0)
+			handlerFixture.AssertNormal(0.0, 1.0, 0.0)
+			handlerFixture.AssertNormal(1.0, 0.0, 0.0)
+			handlerFixture.AssertNormal(0.0, 0.0, 1.0)
 		})
 
 		It("should have scanned the objects", func() {
-			assertObject("MyObject")
+			handlerFixture.AssertObject("MyObject")
 		})
 
 		It("should have scanned the faces", func() {
-			assertFaceStart()
-			assertFaceEnd()
+			handlerFixture.AssertFaceStart()
+			handlerFixture.AssertFaceEnd()
 		})
 
 		It("should have scanned data references", func() {
-			assertCoordReferenceStart()
-			assertVertexIndex(1)
-			assertTexCoordIndex(4)
-			assertNormalIndex(1)
-			assertCoordReferenceEnd()
+			handlerFixture.AssertCoordReferenceStart()
+			handlerFixture.AssertVertexIndex(1)
+			handlerFixture.AssertTexCoordIndex(4)
+			handlerFixture.AssertNormalIndex(1)
+			handlerFixture.AssertCoordReferenceEnd()
 
-			assertCoordReferenceStart()
-			assertVertexIndex(2)
-			assertTexCoordIndex(1)
-			assertNormalIndex(1)
-			assertCoordReferenceEnd()
+			handlerFixture.AssertCoordReferenceStart()
+			handlerFixture.AssertVertexIndex(2)
+			handlerFixture.AssertTexCoordIndex(1)
+			handlerFixture.AssertNormalIndex(1)
+			handlerFixture.AssertCoordReferenceEnd()
 
-			assertCoordReferenceStart()
-			assertVertexIndex(3)
-			assertTexCoordIndex(3)
-			assertNormalIndex(2)
-			assertCoordReferenceEnd()
+			handlerFixture.AssertCoordReferenceStart()
+			handlerFixture.AssertVertexIndex(3)
+			handlerFixture.AssertTexCoordIndex(3)
+			handlerFixture.AssertNormalIndex(2)
+			handlerFixture.AssertCoordReferenceEnd()
 		})
 
 		It("should have scanned material libraries", func() {
-			assertMaterialLibrary("valid_basic.mtl")
+			handlerFixture.AssertMaterialLibrary("valid_basic.mtl")
 		})
 
 		It("should have scanned material references", func() {
-			assertMaterialReference("BlueMaterial")
+			handlerFixture.AssertMaterialReference("BlueMaterial")
 		})
 	})
 
@@ -292,13 +105,14 @@ var _ = Describe("Scanner", func() {
 		})
 
 		It("should have scanned the comments", func() {
-			assertComment("Comment at file start")
-			assertComment("Comment that is right next to special char")
-			assertComment("This comment uses")
-			assertComment("two lines")
-			assertComment("")
-			assertComment("Previous comment was empty. This one contain the # character twice.")
-			assertComment("Comment at file end")
+			handlerFixture.AssertCommentCall("Comment at file start")
+			handlerFixture.AssertCommentCall("Comment that is right next to special char")
+			handlerFixture.AssertCommentCall("This comment uses")
+			handlerFixture.AssertCommentCall("two lines")
+			handlerFixture.AssertCommentCall("")
+			handlerFixture.AssertCommentCall("Previous comment was empty. This one contain the # character twice.")
+			handlerFixture.AssertCommentCall("Comment at file end")
+			handlerFixture.AssertNoMoreCommentCalls()
 		})
 
 	})
---
+++ obj/test_helpers/handler_assertions.go
@@ -0,0 +1,188 @@
+package test_helpers
+
+import (
+	objfakes "github.com/momchil-atanasov/go-data-front/obj/fakes"
+	. "github.com/onsi/gomega"
+)
+
+type HandlerFixture struct {
+	handler                    *objfakes.FakeScannerHandler
+	commentCounter             int
+	vertexStartCounter         int
+	vertexEndCounter           int
+	vertexXCounter             int
+	vertexYCounter             int
+	vertexZCounter             int
+	texCoordStartCounter       int
+	texCoordEndCounter         int
+	texCoordUCounter           int
+	texCoordVCounter           int
+	texCoordWCounter           int
+	normalCounter              int
+	objectCounter              int
+	faceStartCounter           int
+	faceEndCounter             int
+	coordReferenceStartCounter int
+	coordReferenceEndCounter   int
+	vertexIndexCounter         int
+	texCoordIndexCounter       int
+	normalIndexCounter         int
+	materialLibraryCounter     int
+	materialReferenceCounter   int
+}
+
+func NewHandlerFixture() *HandlerFixture {
+	return &HandlerFixture{
+		handler: new(objfakes.FakeScannerHandler),
+	}
+}
+
+func (f *HandlerFixture) Handler() *objfakes.FakeScannerHandler {
+	return f.handler
+}
+
+func (f *HandlerFixture) AssertCommentCall(expectedComment string) {
+	Ω(f.handler.OnCommentCallCount()).Should(BeNumerically(">", f.commentCounter))
+	Ω(f.handler.OnCommentArgsForCall(f.commentCounter)).Should(Equal(expectedComment))
+	f.commentCounter++
+}
+
+func (f *HandlerFixture) AssertNoMoreCommentCalls() {
+	Ω(f.handler.OnCommentCallCount()).Should(Equal(f.commentCounter))
+}
+
+func (f *HandlerFixture) AssertVertexStart() {
+	Ω(f.handler.OnVertexStartCallCount()).Should(BeNumerically(">", f.vertexStartCounter))
+	f.vertexStartCounter++
+}
+
+func (f *HandlerFixture) AssertVertexEnd() {
+	Ω(f.handler.OnVertexEndCallCount()).Should(BeNumerically(">", f.vertexEndCounter))
+	f.vertexEndCounter++
+}
+
+func (f *HandlerFixture) AssertVertexX(expectedX float32) {
+	Ω(f.handler.OnVertexXCallCount()).Should(BeNumerically(">", f.vertexXCounter))
+	Ω(f.handler.OnVertexXArgsForCall(f.vertexXCounter)).Should(BeNumerically("~", expectedX, 0.0001))
+	f.vertexXCounter++
+}
+
+func (f *HandlerFixture) AssertVertexY(expectedY float32) {
+	Ω(f.handler.OnVertexYCallCount()).Should(BeNumerically(">", f.vertexYCounter))
+	Ω(f.handler.OnVertexYArgsForCall(f.vertexYCounter)).Should(BeNumerically("~", expectedY, 0.0001))
+	f.vertexYCounter++
+}
+
+func (f *HandlerFixture) AssertVertexZ(expectedZ float32) {
+	Ω(f.handler.OnVertexZCallCount()).Should(BeNumerically(">", f.vertexZCounter))
+	Ω(f.handler.OnVertexZArgsForCall(f.vertexZCounter)).Should(BeNumerically("~", expectedZ, 0.0001))
+	f.vertexZCounter++
+}
+
+func (f *HandlerFixture) AssertVertexXYZ(expectedX, expectedY, expectedZ float32) {
+	f.AssertVertexStart()
+	f.AssertVertexX(expectedX)
+	f.AssertVertexY(expectedY)
+	f.AssertVertexZ(expectedZ)
+	f.AssertVertexEnd()
+}
+
+func (f *HandlerFixture) AssertTexCoordStart() {
+	Ω(f.handler.OnTexCoordStartCallCount()).Should(BeNumerically(">", f.texCoordStartCounter))
+	f.texCoordStartCounter++
+}
+
+func (f *HandlerFixture) AssertTexCoordU(expectedU float32) {
+	Ω(f.handler.OnTexCoordUCallCount()).Should(BeNumerically(">", f.texCoordUCounter))
+	Ω(f.handler.OnTexCoordUArgsForCall(f.texCoordUCounter)).Should(BeNumerically("~", expectedU, 0.0001))
+	f.texCoordUCounter++
+}
+
+func (f *HandlerFixture) AssertTexCoordV(expectedV float32) {
+	Ω(f.handler.OnTexCoordVCallCount()).Should(BeNumerically(">", f.texCoordVCounter))
+	Ω(f.handler.OnTexCoordVArgsForCall(f.texCoordVCounter)).Should(BeNumerically("~", expectedV, 0.0001))
+	f.texCoordVCounter++
+}
+
+func (f *HandlerFixture) AssertTexCoordEnd() {
+	Ω(f.handler.OnTexCoordEndCallCount()).Should(BeNumerically(">", f.texCoordEndCounter))
+	f.texCoordEndCounter++
+}
+
+func (f *HandlerFixture) AssertTexCoordUV(expectedU, expectedV float32) {
+	f.AssertTexCoordStart()
+	f.AssertTexCoordU(expectedU)
+	f.AssertTexCoordV(expectedV)
+	f.AssertTexCoordEnd()
+}
+
+func (f *HandlerFixture) AssertNormal(expectedX, expectedY, expectedZ float32) {
+	Ω(f.handler.OnNormalCallCount()).Should(BeNumerically(">", f.normalCounter))
+	argX, argY, argZ := f.handler.OnNormalArgsForCall(f.normalCounter)
+	Ω(argX).Should(BeNumerically("~", expectedX, 0.0001))
+	Ω(argY).Should(BeNumerically("~", expectedY, 0.0001))
+	Ω(argZ).Should(BeNumerically("~", expectedZ, 0.0001))
+	f.normalCounter++
+}
+
+func (f *HandlerFixture) AssertObject(expectedName string) {
+	Ω(f.handler.OnObjectCallCount()).Should(BeNumerically(">", f.objectCounter))
+	argName := f.handler.OnObjectArgsForCall(f.objectCounter)
+	Ω(argName).Should(Equal(expectedName))
+	f.objectCounter++
+}
+
+func (f *HandlerFixture) AssertFaceStart() {
+	Ω(f.handler.OnFaceStartCallCount()).Should(BeNumerically(">", f.faceStartCounter))
+	f.faceStartCounter++
+}
+
+func (f *HandlerFixture) AssertFaceEnd() {
+	Ω(f.handler.OnFaceEndCallCount()).Should(BeNumerically(">", f.faceEndCounter))
+	f.faceEndCounter++
+}
+
+func (f *HandlerFixture) AssertCoordReferenceStart() {
+	Ω(f.handler.OnCoordReferenceStartCallCount()).Should(BeNumerically(">", f.coordReferenceStartCounter))
+	f.coordReferenceStartCounter++
+}
+
+func (f *HandlerFixture) AssertCoordReferenceEnd() {
+	Ω(f.handler.OnCoordReferenceEndCallCount()).Should(BeNumerically(">", f.coordReferenceEndCounter))
+	f.coordReferenceEndCounter++
+}
+
+func (f *HandlerFixture) AssertVertexIndex(expectedIndex int) {
+	Ω(f.handler.OnVertexIndexCallCount()).Should(BeNumerically(">", f.vertexIndexCounter))
+	argIndex := f.handler.OnVertexIndexArgsForCall(f.vertexIndexCounter)
+	Ω(argIndex).Should(Equal(expectedIndex))
+	f.vertexIndexCounter++
+}
+
+func (f *HandlerFixture) AssertTexCoordIndex(expectedIndex int) {
+	Ω(f.handler.OnTexCoordIndexCallCount()).Should(BeNumerically(">", f.texCoordIndexCounter))
+	argIndex := f.handler.OnTexCoordIndexArgsForCall(f.texCoordIndexCounter)
+	Ω(argIndex).Should(Equal(expectedIndex))
+	f.texCoordIndexCounter++
+}
+
+func (f *HandlerFixture) AssertNormalIndex(expectedIndex int) {
+	Ω(f.handler.OnNormalIndexCallCount()).Should(BeNumerically(">", f.normalIndexCounter))
+	argIndex := f.handler.OnNormalIndexArgsForCall(f.normalIndexCounter)
+	Ω(argIndex).Should(Equal(expectedIndex))
+	f.normalIndexCounter++
+}
+
+func (f *HandlerFixture) AssertMaterialLibrary(expectedPath string) {
+	Ω(f.handler.OnMaterialLibraryCallCount()).Should(BeNumerically(">", f.materialLibraryCounter))
+	pathArg := f.handler.OnMaterialLibraryArgsForCall(f.materialLibraryCounter)
+	Ω(pathArg).Should(Equal(expectedPath))
+	f.materialLibraryCounter++
+}
+
+func (f *HandlerFixture) AssertMaterialReference(expectedName string) {
+	Ω(f.handler.OnMaterialReferenceCallCount()).Should(BeNumerically(">", f.materialReferenceCounter))
+	nameArg := f.handler.OnMaterialReferenceArgsForCall(f.materialReferenceCounter)
+	Ω(nameArg).Should(Equal(expectedName))
+	f.materialReferenceCounter++
+}
