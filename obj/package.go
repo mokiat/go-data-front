a12ffe5d6237f839e8720e1c3d720fa7e2e195cd
Momchil Atanasov <momchil.dev@gmail.com>
2015-02-04 22:29:57 +0200

    Make the documentation more godocs friendly

--- obj/package.go
+++ obj/package.go
@@ -1,5 +1,5 @@
 /*
-This package provides APIs through which one can parse Wavefront OBJ resources.
+obj package provides APIs through which one can parse Wavefront OBJ resources.
 The OBJ file format is quite common and is used to store 3D model data.
 
 The parsers provided by this library do not support the full OBJ spec, which
@@ -10,85 +10,83 @@ package obj
 import "io"
 
 /*
-An implementation of this interface should be passed to the constructor
-of the `Scanner` interface.
-
-Method of this interface will be called during scanning.
+ScannerHandler interface needs to be implemented by users and passed
+to the Scanner construtor so that the user may receive parsing events.
 */
 type ScannerHandler interface {
 
-	// This method is called when a comment section has been parsed.
+	// OnComment is called when a comment section has been parsed.
 	OnComment(comment string) error
 
-	// This method is called when a material library dependency is parsed.
+	// OnMaterialLibrary is called when a material library dependency is parsed.
 	OnMaterialLibrary(path string) error
 
-	// This method is called when a material reference has been parsed.
+	// OnMaterialReference is called when a material reference has been parsed.
 	OnMaterialReference(name string) error
 
-	// This method is called when a vertex is about to be parsed.
+	// OnVertexStart is called when a vertex is about to be parsed.
 	OnVertexStart() error
 
-	// This method is called when the X coordinate of a vertex is parsed.
+	// OnVertexX is called when the X coordinate of a vertex is parsed.
 	OnVertexX(x float32) error
 
-	// This method is called when the Y coordinate of a vertex is parsed.
+	// OnVertexY is called when the Y coordinate of a vertex is parsed.
 	OnVertexY(y float32) error
 
-	// This method is called when the Z coordinate of a vertex is parsed.
+	// OnVertexZ is called when the Z coordinate of a vertex is parsed.
 	OnVertexZ(z float32) error
 
-	// This method is called when the W coordinate of a vertex is parsed.
+	// OnVertexW is called when the W coordinate of a vertex is parsed.
 	OnVertexW(w float32) error
 
-	// This method is called when the parsing of a given vertex has finished.
+	// OnVertexEnd is called when the parsing of a given vertex has finished.
 	OnVertexEnd() error
 
-	// This method is called when a texture coordinate is about to be parsed.
+	// OnTexCoordStart is called when a texture coordinate is about to be parsed.
 	OnTexCoordStart() error
 
-	// This method is called when the U coordinate of a texture coordinate is parsed.
+	// OnTexCoordU is called when the U coordinate of a texture coordinate is parsed.
 	OnTexCoordU(u float32) error
 
-	//  This method is called when the V coordinate of a texture coordinate is parsed.
+	// OnTexCoordV is called when the V coordinate of a texture coordinate is parsed.
 	OnTexCoordV(v float32) error
 
-	//  This method is called when the W coordinate of a texture coordinate is parsed.
+	// OnTexCoordW is called when the W coordinate of a texture coordinate is parsed.
 	OnTexCoordW(w float32) error
 
-	// This method is called when a texture coordinate has been fully parsed.
+	// OnTexCoordEnd is called when a texture coordinate has been fully parsed.
 	OnTexCoordEnd() error
 
-	// This method is called when a normal has been parsed.
+	// OnNormal is called when a normal has been parsed.
 	OnNormal(x, y, z float32) error
 
-	// This method is called when a new object declaration is parsed.
+	// OnObject is called when a new object declaration is parsed.
 	OnObject(name string) error
 
-	// This method is called when a new face is about to be parsed.
+	// OnFaceStart is called when a new face is about to be parsed.
 	OnFaceStart() error
 
-	// This method is called when a new coord reference is about to be parsed.
+	// OnCoordReferenceStart is called when a new coord reference is about to be parsed.
 	OnCoordReferenceStart() error
 
-	// This method is called when a vertex index reference is parsed.
+	// OnVertexIndex is called when a vertex index reference is parsed.
 	OnVertexIndex(index int) error
 
-	// This method is called when a texture coordinate index reference is parsed.
+	// OnTexCoordIndex is called when a texture coordinate index reference is parsed.
 	OnTexCoordIndex(index int) error
 
-	// This method is called when a normal index reference is parsed.
+	// OnNormalIndex is called when a normal index reference is parsed.
 	OnNormalIndex(index int) error
 
-	// This method is called when the parsing of a coord reference has finished.
+	// OnCoordReferenceEnd is called when the parsing of a coord reference has finished.
 	OnCoordReferenceEnd() error
 
-	// This method is called when a face has been fully parsed.
+	// OnFaceEnd is called when a face has been fully parsed.
 	OnFaceEnd() error
 }
 
 /*
-The Scanner interface represents an event-based parser. The model data
+Scanner interface represents an event-based parser. The model data
 is iterated - an interesting element at a time - and an event is thrown.
 
 Events are sent to the user via method invocations on the ScannerHandler
@@ -96,14 +94,14 @@ interface that is previously specified.
 */
 type Scanner interface {
 
-	// Reads an OBJ resource from the specified io.Reader stream.
+	// Scan parses an OBJ resource from the specified io.Reader stream.
 	// An error is returned should parsing fail for some reason.
 	Scan(io.Reader) error
 }
 
 /*
-Creates a new Scanner using the specified ScannerHandler for callback
-handling.
+NewScanner creates a new Scanner using the specified ScannerHandler
+for callback handling.
 */
 func NewScanner(handler ScannerHandler) Scanner {
 	return &scanner{
