93c603ff1c53a1013802f5aa7658ce298bf7091c
Momchil Atanasov <momchil.dev@gmail.com>
2015-02-28 17:57:49 +0200

    Add OBJ decoder implementation

---
+++ decoder/obj/decoder.go
@@ -0,0 +1,258 @@
+package obj
+
+import (
+	"errors"
+	"io"
+
+	"github.com/momchil-atanasov/go-data-front/common"
+	scanOBJ "github.com/momchil-atanasov/go-data-front/scanner/obj"
+)
+
+// DecodeLimits specifies restrictions on parsing.
+//
+// One will generally use this to limit the number of
+// data that is parsed in order to prevent out of memory
+// errors
+type DecodeLimits struct {
+
+	// MaxVertexCount specifies the maximum number of vertices
+	// that can be parsed before an error is thrown.
+	MaxVertexCount int
+
+	// MaxTexCoordCount specifies the maximum number of texture
+	// coordinates that can be parsed before an error is thrown.
+	MaxTexCoordCount int
+
+	// MaxNormalCount specifies the maximum number of normals
+	// that can be parsed before an error is thrown.
+	MaxNormalCount int
+
+	// MaxObjectCount specifies the maximum number of objects
+	// that can be parsed before an error is thrown.
+	MaxObjectCount int
+
+	// MaxMaterialLibraryCount specifies the maximum number of
+	// material library references that can be parsed before
+	// an error is thrown.
+	MaxMaterialLibraryCount int
+}
+
+// Decoder is an API that allows one to decode OBJ
+// Wavefront resources into an object model.
+type Decoder interface {
+
+	// Decode decodes the OBJ Wavefront resource, specified
+	// through the io.Reader, into a Library model.
+	//
+	// If decoding fails for some reason, an error is returned.
+	Decode(io.Reader) (*Model, error)
+}
+
+func NewDecoder(limits DecodeLimits) Decoder {
+	return &decoder{
+		limits:  &limits,
+		scanner: scanOBJ.NewScanner(),
+	}
+}
+
+type decoder struct {
+	limits  *DecodeLimits
+	scanner common.Scanner
+}
+
+func (d *decoder) Decode(reader io.Reader) (*Model, error) {
+	context := newDecodeContext(d.limits)
+	err := d.scanner.Scan(reader, context.HandleEvent)
+	if err != nil {
+		return nil, err
+	}
+	return context.Model, nil
+}
+
+func newDecodeContext(limits *DecodeLimits) *decodeContext {
+	return &decodeContext{
+		Limits:        limits,
+		Model:         new(Model),
+		CurrentObject: nil,
+		CurrentMesh:   nil,
+		CurrentFace:   nil,
+	}
+}
+
+type decodeContext struct {
+	Limits           *DecodeLimits
+	Model            *Model
+	CurrentObject    *Object
+	CurrentMesh      *Mesh
+	CurrentFace      *Face
+	CurrentReference *Reference
+}
+
+func (c *decodeContext) HandleEvent(event common.Event) error {
+	switch actual := event.(type) {
+	case scanOBJ.MaterialLibraryEvent:
+		return c.handleMaterialLibrary(actual)
+	case scanOBJ.VertexEvent:
+		return c.handleVertex(actual)
+	case scanOBJ.NormalEvent:
+		return c.handleNormal(actual)
+	case scanOBJ.TexCoordEvent:
+		return c.handleTexCoord(actual)
+	case scanOBJ.ObjectEvent:
+		return c.handleObject(actual)
+	case scanOBJ.MaterialReferenceEvent:
+		return c.handleMaterialReference(actual)
+	case scanOBJ.FaceStartEvent:
+		return c.handleFaceStart()
+	case scanOBJ.FaceEndEvent:
+		return c.handleFaceEnd()
+	case scanOBJ.ReferenceSetStartEvent:
+		return c.handleReferencesStart()
+	case scanOBJ.ReferenceSetEndEvent:
+		return c.handleReferencesEnd()
+	case scanOBJ.VertexReferenceEvent:
+		return c.handleVertexReference(actual)
+	case scanOBJ.TexCoordReferenceEvent:
+		return c.handleTexCoordReference(actual)
+	case scanOBJ.NormalReferenceEvent:
+		return c.handleNormalReference(actual)
+	}
+	return nil
+}
+
+func (c *decodeContext) handleMaterialLibrary(event scanOBJ.MaterialLibraryEvent) error {
+	if len(c.Model.MaterialLibraries) >= c.Limits.MaxMaterialLibraryCount {
+		return errors.New("Maximum number of material libraries reached!")
+	}
+	c.Model.MaterialLibraries = append(c.Model.MaterialLibraries, event.FilePath)
+	return nil
+}
+
+func (c *decodeContext) handleVertex(event scanOBJ.VertexEvent) error {
+	if len(c.Model.Vertices) >= c.Limits.MaxVertexCount {
+		return errors.New("Maximum number of vertices reached!")
+	}
+	c.Model.Vertices = append(c.Model.Vertices, Vertex{
+		X: event.X,
+		Y: event.Y,
+		Z: event.Z,
+		W: event.W,
+	})
+	return nil
+}
+
+func (c *decodeContext) handleTexCoord(event scanOBJ.TexCoordEvent) error {
+	if len(c.Model.TexCoords) >= c.Limits.MaxTexCoordCount {
+		return errors.New("Maximum number of texture coordinates reached!")
+	}
+	c.Model.TexCoords = append(c.Model.TexCoords, TexCoord{
+		U: event.U,
+		V: event.V,
+		W: event.W,
+	})
+	return nil
+}
+
+func (c *decodeContext) handleNormal(event scanOBJ.NormalEvent) error {
+	if len(c.Model.Normals) >= c.Limits.MaxNormalCount {
+		return errors.New("Maximum number of normals reached!")
+	}
+	c.Model.Normals = append(c.Model.Normals, Normal{
+		X: event.X,
+		Y: event.Y,
+		Z: event.Z,
+	})
+	return nil
+}
+
+func (c *decodeContext) handleObject(event scanOBJ.ObjectEvent) error {
+	if len(c.Model.Objects) >= c.Limits.MaxObjectCount {
+		return errors.New("Maximum number of objects reached!")
+	}
+	c.CurrentMesh = nil
+	c.CurrentObject = new(Object)
+	c.CurrentObject.Name = event.ObjectName
+	c.Model.Objects = append(c.Model.Objects, c.CurrentObject)
+	return nil
+}
+
+func (c *decodeContext) handleMaterialReference(event scanOBJ.MaterialReferenceEvent) error {
+	c.assureCurrentObject()
+	c.CurrentMesh = new(Mesh)
+	c.CurrentMesh.MaterialName = event.MaterialName
+	c.CurrentObject.Meshes = append(c.CurrentObject.Meshes, c.CurrentMesh)
+	return nil
+}
+
+func (c *decodeContext) handleFaceStart() error {
+	c.assureCurrentMesh()
+	c.CurrentFace = new(Face)
+	return nil
+}
+
+func (c *decodeContext) handleFaceEnd() error {
+	if len(c.CurrentFace.References) < 3 {
+		return errors.New("Face needs to have at least three vertices.")
+	}
+	c.CurrentMesh.Faces = append(c.CurrentMesh.Faces, c.CurrentFace)
+	return nil
+}
+
+func (c *decodeContext) handleReferencesStart() error {
+	c.CurrentReference = &Reference{
+		TexCoordIndex: UndefinedIndex,
+		NormalIndex:   UndefinedIndex,
+	}
+	return nil
+}
+
+func (c *decodeContext) handleReferencesEnd() error {
+	c.CurrentFace.References = append(c.CurrentFace.References, *c.CurrentReference)
+	return nil
+}
+
+func (c *decodeContext) handleVertexReference(event scanOBJ.VertexReferenceEvent) error {
+	if event.VertexIndex > 0 {
+		c.CurrentReference.VertexIndex = event.VertexIndex - 1
+	} else {
+		c.CurrentReference.VertexIndex = int64(len(c.Model.Vertices)) + event.VertexIndex
+	}
+	return nil
+}
+
+func (c *decodeContext) handleTexCoordReference(event scanOBJ.TexCoordReferenceEvent) error {
+	if event.TexCoordIndex > 0 {
+		c.CurrentReference.TexCoordIndex = event.TexCoordIndex - 1
+	} else {
+		c.CurrentReference.TexCoordIndex = int64(len(c.Model.TexCoords)) + event.TexCoordIndex
+	}
+	return nil
+}
+
+func (c *decodeContext) handleNormalReference(event scanOBJ.NormalReferenceEvent) error {
+	if event.NormalIndex > 0 {
+		c.CurrentReference.NormalIndex = event.NormalIndex - 1
+	} else {
+		c.CurrentReference.NormalIndex = int64(len(c.Model.Normals)) + event.NormalIndex
+	}
+	return nil
+}
+
+func (c *decodeContext) assureCurrentObject() {
+	if c.CurrentObject != nil {
+		return
+	}
+	c.CurrentObject = &Object{
+		Name: "Default",
+	}
+	c.Model.Objects = append(c.Model.Objects, c.CurrentObject)
+}
+
+func (c *decodeContext) assureCurrentMesh() {
+	if c.CurrentMesh != nil {
+		return
+	}
+	c.assureCurrentObject()
+	c.CurrentMesh = new(Mesh)
+	c.CurrentObject.Meshes = append(c.CurrentObject.Meshes, c.CurrentMesh)
+}
---
+++ decoder/obj/decoder_test.go
@@ -0,0 +1,380 @@
+package obj_test
+
+import (
+	"fmt"
+	"os"
+
+	. "github.com/momchil-atanasov/go-data-front/decoder/obj"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Decoder", func() {
+	var decoder Decoder
+	var model *Model
+	var decodeErr error
+
+	decodeFile := func(filename string) {
+		file, err := os.Open(fmt.Sprintf("obj_test_res/%s", filename))
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		model, decodeErr = decoder.Decode(file)
+	}
+
+	itShouldHaveReturnedAnError := func() {
+		It("should have returned an error", func() {
+			Ω(decodeErr).Should(HaveOccurred())
+		})
+	}
+
+	itShouldNotHaveReturnedAnError := func() {
+		It("should not have returned an error", func() {
+			Ω(decodeErr).ShouldNot(HaveOccurred())
+		})
+	}
+
+	BeforeEach(func() {
+		limits := DecodeLimits{
+			MaxVertexCount:          256,
+			MaxTexCoordCount:        256,
+			MaxNormalCount:          256,
+			MaxObjectCount:          256,
+			MaxMaterialLibraryCount: 256,
+		}
+		decoder = NewDecoder(limits)
+	})
+
+	Context("when a basic file is decoded", func() {
+		BeforeEach(func() {
+			decodeFile("valid_basic.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have returned a non-nil model", func() {
+			Ω(model).ShouldNot(BeNil())
+		})
+
+		It("should have decoded material libraries", func() {
+			Ω(model.MaterialLibraries).Should(HaveLen(1))
+			Ω(model.MaterialLibraries[0]).Should(Equal("materials.mtl"))
+		})
+
+		It("should have decoded vertices", func() {
+			Ω(model.Vertices).Should(HaveLen(4))
+			Ω(model.Vertices[0]).Should(Equal(Vertex{
+				X: 0.1, Y: 0.2, Z: 0.3, W: 1.0,
+			}))
+			Ω(model.Vertices[1]).Should(Equal(Vertex{
+				X: 0.4, Y: 0.5, Z: 0.6, W: 1.0,
+			}))
+			Ω(model.Vertices[2]).Should(Equal(Vertex{
+				X: 0.7, Y: 0.8, Z: 0.9, W: 1.0,
+			}))
+			Ω(model.Vertices[3]).Should(Equal(Vertex{
+				X: 1.0, Y: 0.9, Z: 0.8, W: 1.0,
+			}))
+		})
+
+		It("should have decoded normals", func() {
+			Ω(model.Normals).Should(HaveLen(3))
+			Ω(model.Normals[0]).Should(Equal(Normal{
+				X: 0.0, Y: 1.0, Z: 0.0,
+			}))
+			Ω(model.Normals[1]).Should(Equal(Normal{
+				X: 1.0, Y: 0.0, Z: 0.0,
+			}))
+			Ω(model.Normals[2]).Should(Equal(Normal{
+				X: 0.0, Y: 0.0, Z: 1.0,
+			}))
+		})
+
+		It("should have decoded texture coordinates", func() {
+			Ω(model.TexCoords).Should(HaveLen(4))
+			Ω(model.TexCoords[0]).Should(Equal(TexCoord{
+				U: 0.1, V: 0.2, W: 0.3,
+			}))
+			Ω(model.TexCoords[1]).Should(Equal(TexCoord{
+				U: 0.4, V: 0.5, W: 0.6,
+			}))
+			Ω(model.TexCoords[2]).Should(Equal(TexCoord{
+				U: 0.7, V: 0.8, W: 0.9,
+			}))
+			Ω(model.TexCoords[3]).Should(Equal(TexCoord{
+				U: 1.0, V: 0.9, W: 0.8,
+			}))
+		})
+
+		It("should have decoded objects", func() {
+			Ω(model.Objects).Should(HaveLen(1))
+			Ω(model.Objects[0].Name).Should(Equal("MyObject"))
+		})
+
+		It("should have decoded meshes", func() {
+			object := model.Objects[0]
+			Ω(object.Meshes).Should(HaveLen(1))
+			Ω(object.Meshes[0].MaterialName).Should(Equal("BlueMaterial"))
+		})
+
+		It("should have decoded faces", func() {
+			mesh := model.Objects[0].Meshes[0]
+			Ω(mesh.Faces).Should(HaveLen(2))
+		})
+
+		It("should have decoded references", func() {
+			face := model.Objects[0].Meshes[0].Faces[0]
+			Ω(face.References).Should(HaveLen(3))
+
+			Ω(face.References[0]).Should(Equal(Reference{
+				VertexIndex:   0,
+				TexCoordIndex: 3,
+				NormalIndex:   0,
+			}))
+			Ω(face.References[0].HasTexCoord()).Should(BeTrue())
+			Ω(face.References[0].HasNormal()).Should(BeTrue())
+
+			Ω(face.References[1]).Should(Equal(Reference{
+				VertexIndex:   1,
+				TexCoordIndex: 0,
+				NormalIndex:   0,
+			}))
+			Ω(face.References[1].HasTexCoord()).Should(BeTrue())
+			Ω(face.References[1].HasNormal()).Should(BeTrue())
+
+			Ω(face.References[2]).Should(Equal(Reference{
+				VertexIndex:   2,
+				TexCoordIndex: 2,
+				NormalIndex:   1,
+			}))
+			Ω(face.References[2].HasTexCoord()).Should(BeTrue())
+			Ω(face.References[2].HasNormal()).Should(BeTrue())
+		})
+	})
+
+	Context("when a file with multiple objects is decoded", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_objects.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have decoded all objects", func() {
+			Ω(model.Objects).Should(HaveLen(2))
+			Ω(model.Objects[0].Name).Should(Equal("First"))
+			Ω(model.Objects[1].Name).Should(Equal("Second"))
+		})
+
+		Context("when the number of objects is larger than the limit", func() {
+			BeforeEach(func() {
+				limits := DecodeLimits{
+					MaxObjectCount: 1,
+				}
+				decoder = NewDecoder(limits)
+			})
+
+			itShouldHaveReturnedAnError()
+		})
+	})
+
+	Context("when a file with all kinds of vertices is decoded", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_vertices.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have decoded all vertices", func() {
+			Ω(model.Vertices).Should(HaveLen(2))
+			Ω(model.Vertices[0]).Should(Equal(Vertex{
+				X: 1.0, Y: 2.0, Z: 3.0, W: 1.0,
+			}))
+			Ω(model.Vertices[1]).Should(Equal(Vertex{
+				X: 4.0, Y: 5.0, Z: 6.0, W: 7.0,
+			}))
+		})
+
+		Context("when the number of vertices is larger than the limit", func() {
+			BeforeEach(func() {
+				limits := DecodeLimits{
+					MaxVertexCount: 1,
+				}
+				decoder = NewDecoder(limits)
+			})
+
+			itShouldHaveReturnedAnError()
+		})
+	})
+
+	Context("when a file with all kinds of texture coordinates is decoded", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_tex_coords.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have decoded all texture coordinates", func() {
+			Ω(model.TexCoords).Should(HaveLen(3))
+			Ω(model.TexCoords[0]).Should(Equal(TexCoord{
+				U: 0.1, V: 0.0, W: 0.0,
+			}))
+			Ω(model.TexCoords[1]).Should(Equal(TexCoord{
+				U: 0.4, V: 0.5, W: 0.0,
+			}))
+			Ω(model.TexCoords[2]).Should(Equal(TexCoord{
+				U: 0.7, V: 0.8, W: 0.9,
+			}))
+		})
+
+		Context("when the number of texture coordinates is larger than the limit", func() {
+			BeforeEach(func() {
+				limits := DecodeLimits{
+					MaxTexCoordCount: 1,
+				}
+				decoder = NewDecoder(limits)
+			})
+
+			itShouldHaveReturnedAnError()
+		})
+	})
+
+	Context("when a file with all kinds of normals is decoded", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_normals.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have decoded all normals", func() {
+			Ω(model.Normals).Should(HaveLen(2))
+			Ω(model.Normals[0]).Should(Equal(Normal{
+				X: 1.0, Y: -2.0, Z: 3.0,
+			}))
+			Ω(model.Normals[1]).Should(Equal(Normal{
+				X: -0.5, Y: 0.4, Z: -0.7,
+			}))
+		})
+
+		Context("when the number of normals is larger than the limit", func() {
+			BeforeEach(func() {
+				limits := DecodeLimits{
+					MaxNormalCount: 1,
+				}
+				decoder = NewDecoder(limits)
+			})
+
+			itShouldHaveReturnedAnError()
+		})
+	})
+
+	Context("when a file with all kinds of material libraries is scanned", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_material_libraries.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have decoded all material libraries", func() {
+			Ω(model.MaterialLibraries).Should(HaveLen(3))
+			Ω(model.MaterialLibraries[0]).Should(Equal("first.mtl"))
+			Ω(model.MaterialLibraries[1]).Should(Equal("second.mtl"))
+			Ω(model.MaterialLibraries[2]).Should(Equal("third.mtl"))
+		})
+
+		Context("when the number of material libraries is larger than the limit", func() {
+			BeforeEach(func() {
+				limits := DecodeLimits{
+					MaxMaterialLibraryCount: 1,
+				}
+				decoder = NewDecoder(limits)
+			})
+
+			itShouldHaveReturnedAnError()
+		})
+	})
+
+	Context("when an object without a mesh is scanned", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_no_mesh.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have created a default mesh", func() {
+			meshes := model.Objects[0].Meshes
+			Ω(meshes).Should(HaveLen(1))
+			Ω(meshes[0]).ShouldNot(BeNil())
+			Ω(meshes[0].MaterialName).Should(Equal(""))
+		})
+	})
+
+	Context("when faces without an object are scanned", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_no_object.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have created a default object", func() {
+			Ω(model.Objects).Should(HaveLen(1))
+			Ω(model.Objects[0].Name).Should(Equal("Default"))
+
+			Ω(model.Objects[0].Meshes).Should(HaveLen(1))
+			Ω(model.Objects[0].Meshes[0].MaterialName).Should(Equal("Red"))
+		})
+	})
+
+	Context("when faces without an object and mesh are scanned", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_no_object_no_mesh.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("should have created a default object and default mesh", func() {
+			Ω(model.Objects).Should(HaveLen(1))
+			Ω(model.Objects[0].Name).Should(Equal("Default"))
+
+			Ω(model.Objects[0].Meshes).Should(HaveLen(1))
+			Ω(model.Objects[0].Meshes[0].MaterialName).Should(Equal(""))
+		})
+	})
+
+	Context("when negative indices are scanned", func() {
+		JustBeforeEach(func() {
+			decodeFile("valid_negative_indices.obj")
+		})
+
+		itShouldNotHaveReturnedAnError()
+
+		It("have normalized the references", func() {
+			face := model.Objects[0].Meshes[0].Faces[0]
+			Ω(face.References).Should(HaveLen(3))
+			Ω(face.References[0]).Should(Equal(Reference{
+				VertexIndex:   0,
+				TexCoordIndex: 3,
+				NormalIndex:   0,
+			}))
+			Ω(face.References[1]).Should(Equal(Reference{
+				VertexIndex:   1,
+				TexCoordIndex: 0,
+				NormalIndex:   0,
+			}))
+			Ω(face.References[2]).Should(Equal(Reference{
+				VertexIndex:   2,
+				TexCoordIndex: 2,
+				NormalIndex:   1,
+			}))
+		})
+	})
+
+	Context("when decoding face without enough references", func() {
+		BeforeEach(func() {
+			decodeFile("error_missing_face_data.obj")
+		})
+
+		itShouldHaveReturnedAnError()
+	})
+})
